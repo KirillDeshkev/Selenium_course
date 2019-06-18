7942e693adcfa8bb69af16e3b2e31d3bc5e0e276
KirillDeshkev <kirill.deshkev@gmail.com>
2019-06-18 21:25:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('http://www.minsktrans.by/lookout_yard/Home/Index/minsk?tt=bus&r=1&d=0&s=9499')
