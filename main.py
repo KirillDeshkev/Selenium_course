6961a718ad79eaa91a8528bcf93803250201a723
KirillDeshkev <kirill.deshkev@gmail.com>
2019-06-30 14:04:52 +0300

    Complete parametrisation tasks

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
-opt = webdriver.ChromeOptions()
-opt.add_experimental_option('w3c', False)
-browser = webdriver.Chrome(chrome_options=opt)
-browser.get("http://suninjuly.github.io/wait2.html")
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+driver = webdriver.Firefox()
 
-# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
-button = WebDriverWait(browser, 5).until(
-    EC.element_to_be_clickable((By.ID, "check"))
-)
-button.click()
-message = browser.find_element_by_id("check_message")
-
-assert "успешно" in message.text
+driver.get("https://stepik.org/lesson/25969/step/8")
